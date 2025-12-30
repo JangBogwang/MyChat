907d12f3e48b494f24565d308687a0f7d6b6226a
unknown <wkdqhrhkd12@gmail.com>
2025-12-30 10:59:58 +0900

    feat: 에러 수정

--- BE/app/config/Settings.py
+++ BE/app/config/Settings.py
@@ -1,13 +1,23 @@
 from pydantic_settings import BaseSettings
 
-# 로컬 환경일 경우 환경변수 주입 
 class Settings(BaseSettings):
+    # LLM & Vector DB
     OPENAI_API_KEY: str
     QDRANT_HOST: str
     QDRANT_PORT: int
+    
+    # Chatbot config
     MAIN_SENDER: str
+
+    # Database
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: str
+    DB_NAME: str
 
     class Config:
         env_file = ".env"
+        env_file_encoding = 'utf-8'
 
 settings = Settings()
